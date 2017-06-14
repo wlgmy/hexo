a31da10ea671402ed2a853c8b7cb4d196d9a86b1
wlgmy <991422465@qq.com>
2017-06-14 16:12:53 +0800

    add deploy in 11.214

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,14 @@ case $OPT in
   hexo d
   echo "commit to git"
   git add . && git commit -m"a new deploy"
+  echo "push to git"
+  git push
+  echo "deploy to 112.124.11.214"
+  ssh root@112.124.11.214 'sh /usr/share/nginx/hexo/deploy.sh pull'
   ;;
   pull|Pull|p|P)
   echo "git pull"
+  git pull
   ;;
   *)
   useage
